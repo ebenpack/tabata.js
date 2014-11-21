500272cf75e1fa4afae17d16ab793e30f71c603b
ebenpack <ebenpackwood@gmail.com>
2014-11-20 19:02:59 -0500

    Add display formatting

--- src/tabata.js
+++ src/tabata.js
@@ -8,25 +8,49 @@ function Tabata(timer, delay){
     var lastUpdate = 0;
     var timeoutId;
     var delay = typeof delay === 'undefined' ? 200 : delay;
+    var eventQueue = [];
+    var eventCursor = 0;
     var self = this;
     self.time = 0;
+
+    // TODO: parse timer object to create event queue.
+    //
     // Update total elapsed time.
     // Fire 'second' events every second-ish while running.
     // Also check for and fire other events.
-    var update = function(){
+    function update(){
         var now = Date.now();
         var elapsed = now - lastUpdate;
         self.time += elapsed;
-        var timeSeconds = self.second();
+        var timeSeconds = Math.floor(self.time / 1000);
         if (timeSeconds - second >= 1){
             second = timeSeconds;
             self.fire('second');
+            // TODO: Check next item in event queue and fire events
         }
         lastUpdate = now;
         timeoutId = window.setTimeout(update, delay);
-    };
+    }
+    function parseTimer(){
+        for (var i = 0, len = timer.length; i < len; i++){
+            // Parse some stuff
+        }
+    }
+    function formatTime(time) {
+        return pad(Math.floor(time / 60), 2) + ":" + pad(time % 60, 2);
+    }
+    function pad(num, size){
+        var numStr = num.toString();
+        while (numStr.length < size) {
+            numStr = '0' + numStr;
+        }
+        return numStr;
+    }
     self.second = function(){
-        return Math.floor(self.time / 1000);
+        return formatTime(second);
+    };
+    self.centisecond = function(){
+        return formatTime(parseFloat((self.time / 1000).toFixed(2)));
     };
     self.start = function(){
         lastUpdate = Date.now();
